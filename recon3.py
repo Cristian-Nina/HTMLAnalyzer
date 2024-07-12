3db4ee68fd1ca8cc7faeefb688c078c9335f794d
Cristian Nina <110742774+Cristian-Nina@users.noreply.github.com>
2024-07-12 15:05:23 -0300

    Create recon3.py

---
+++ recon3.py
@@ -0,0 +1,41 @@
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.firefox.service import Service
+from selenium.common.exceptions import WebDriverException
+import time
+
+# Configurar opciones para navegación en modo headless
+options = Options()
+options.headless = True
+service = Service(executable_path=r'C:\\Users\\nmalizia\\Downloads\\geckodriver-v0.34.0-win32\\geckodriver.exe')
+driver = webdriver.Firefox(options=options, service=service)
+
+# Palabras clave a buscar
+keywords = ["access banking", "usuario", "clave", "icbc", "ingresar"]
+
+def main():
+    with open('potentialphishing.txt') as file:
+        websites = file.readlines()
+    for website in websites:
+        website = website.strip()
+        check_website(website)
+
+def check_website(url):
+    try:
+        driver.get(url)
+        time.sleep(3)  # Esperar a que la página cargue
+        html = driver.page_source.lower()  # Obtener el HTML en minúsculas para facilitar la búsqueda
+
+        # Buscar palabras clave
+        if all(keyword in html for keyword in keywords):
+            print(f'Palabras clave encontradas en: {url}')
+            with open("links.txt", "a") as file:  # Cambia a links.txt
+                file.write(f'{url}\n')
+        else:
+            print(f'No se encontraron palabras clave en: {url}')
+    except WebDriverException:
+        print(f'{url} es una URL inválida')
+
+if __name__ == "__main__":
+    main()
+    driver.quit()
